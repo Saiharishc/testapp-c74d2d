8b1f47687fee7934d1df73a2a00eb08827042ba8
Saiharishc <51015572+Saiharishc@users.noreply.github.com>
2026-07-06 08:15:39 -0500

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TestApp</h1>
+        <p>Welcome to your TestApp!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
